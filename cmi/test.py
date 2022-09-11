40fa4979665e3ec3f73c5cfa2bacf2b6f48846b6
Alexander Dreweke <alexander@dreweke.net>
2022-09-11 20:56:59 +0200

    dump all data to disk

--- cmi/test.py
+++ cmi/test.py
@@ -23,24 +23,38 @@ def main() -> None:
     parser.add_argument('--password', default='data', type=str, help='Password to authenticate with')
     parser.add_argument('--after', default='1970-01-01', type=str, help='only import data that was created after (YYYY-MM-DD)')
     parser.add_argument('--before', default=now.strftime('%Y-%m-%d'), type=str, help='only import data that was created before (YYYY-MM-DD)')
-    parser.add_argument('--output', default='dump', type=str, help='folder to write data dump to')
+    parser.add_argument('--output', default='LOG', type=str, help='folder to write data dump to')
 
     arguments = parser.parse_args()
     before = datetime.datetime.fromisoformat(arguments.before).date()
     after = datetime.datetime.fromisoformat(arguments.after).date()
     print(f'preparing output folder "{arguments.output}"')
     shutil.rmtree(arguments.output, ignore_errors=True)
-    pathlib.Path(f'{arguments.output}/LOG').mkdir(parents=True, exist_ok=True)
+    pathlib.Path(f'{arguments.output}').mkdir(parents=True, exist_ok=True)
 
     print('loading data')
     data = Extractor.process(Configuration(arguments.host, arguments.port, arguments.user, arguments.password, arguments.encoding, after, before, True))
 
     print('dumping infoH raw')
     dump(data.infoH.raw, f'{arguments.output}/infoh.log')
+    with open(f'{arguments.output}/infoh.log_my', 'wb') as f:
+        data.infoH.export(f, arguments.encoding)
 
     print('dumping info raw')
-    dump(data.info.raw, f'{arguments.output}/info.log')
-    
+    dump(data.info.raw, f'{arguments.output}/info{data.infoH.folder}.log')
+    with open(f'{arguments.output}/info{data.infoH.folder}.log_my', 'wb') as f:
+        data.info.export(f, arguments.encoding)
+
+    print('dumping event_groups raw')
+    for group in data.groups:
+        time = group.events[0].time
+        filename = time.strftime('data_%Y_%m_%d_%H_%M_%S.log')
+        pathlib.Path(f'{arguments.output}/{time.year}').mkdir(parents=True, exist_ok=True)
+        dump(group.raw, f'{arguments.output}/{time.year}/{filename}')
+        with open(f'{arguments.output}/{time.year}/{filename}', 'wb') as f:
+            group.export(f, arguments.encoding)
+
+
 
 if __name__ == '__main__':
     main()
