0bfaed0e056cce57d6033efd8d0ab3cbded68989
Alexander Dreweke <alexander@dreweke.net>
2024-09-15 10:42:24 +0200

    be quiet on poetry commands

--- bin/.script.sh
+++ bin/.script.sh
@@ -9,4 +9,4 @@ THIS_NAME="$(basename "${THIS_PATH}")"
 THIS_DIR="$(dirname "${THIS_PATH}")"
 
 cd "${THIS_DIR}/.."
-poetry run cmi/$(basename ${0}).py $@
+poetry run --quiet ./cmi/$(basename ${0}).py $@
